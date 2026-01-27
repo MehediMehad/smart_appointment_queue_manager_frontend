c1bb880ca7963febbf72ec61bf7e0664198a0719
Mehedi Mehad <mdmehedihasanmehad@gmail.com>
2026-01-27 19:01:26 +0600

    fix error

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,10 +1,4 @@
-import { getDashboardSummary } from "@/actions/dashboard";
-
 const ServicesPage = async () => {
-  const data = await getDashboardSummary();
-
-  console.log("llllllllllllllll", { data });
-
   return (
     <div>
       <h1>This is ServicesPage component</h1>
